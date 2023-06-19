0b2b3b49bb0e8ebe431bb438ada18c475ecb71ff
mingzi <mingzi47@qq.com>
2023-06-19 20:28:49 +0800

    finshed skiplist(insert, contains, erase, [], iterator)

--- SkipList/skiplist.hpp
+++ SkipList/skiplist.hpp
@@ -1,40 +1,148 @@
+#include <array>
+#include <iostream>
 #include <memory>
+#include <random>
+#include <utility>
+#include <vector>
 
 namespace mzi {
 // \frac{1}{4}
 constexpr int P = 4;
 constexpr int S = 0xFFFF;
-constexpr int PS = P / S;
+constexpr int PS = S / P;
 constexpr int MAX_LEV = 32;
 
-template <typename K, typename V, typename Comp = std::less<K>()>
+template <typename K, typename V, typename Comp = std::less<K>>
 struct SkipList {
-  using SNode = struct {};
+
+  struct SkipListNode {
+    K key;
+    V value;
+    std::vector<std::shared_ptr<SkipListNode>> forward;
+    SkipListNode(K key, V value, int level) : key(key), value(value) {
+      forward.reserve(level + 1);
+    }
+  };
+
+  using Nptr = std::shared_ptr<SkipListNode>;
+
+  struct Iter {
+    Nptr value;
+    // ++iter
+    auto operator++() -> Iter { return *this = Iter{value->forward[0]}; }
+    // iter++
+    auto operator++(int) -> Iter {
+      auto res = Iter{value};
+      value = value->forward[0];
+      return res;
+    }
+    auto operator*() -> std::pair<K, V> { return {value->key, value->value}; }
+  };
 
 private:
+  Nptr head, tail;
+  int cur_level;
+  Comp cmp;
+  std::array<Nptr, MAX_LEV + 1> update{};
+  std::mt19937 rng{std::random_device{}()};
+
 public:
   SkipList();
   auto random_level() -> int;
-  auto find(K key) -> V;
+  auto find(K key) -> std::pair<Nptr, bool>;
   auto insert(K key, V value) -> bool;
   auto erase(K key) -> bool;
-  auto begin() -> V;
-  auto end() -> V;
-  auto operator[](K key) -> V&;
+  auto begin() -> Iter;
+  auto end() -> Iter;
+  auto operator[](K key) -> V &;
+  auto contains(K key) -> bool;
 };
 
 template <typename K, typename V, typename Comp>
-auto SkipList<K, V, Comp>::random_level() -> int {}
+SkipList<K, V, Comp>::SkipList()
+    : head(std::make_shared<SkipListNode>(K{}, V{}, MAX_LEV)),
+      tail(std::make_shared<SkipListNode>(K{}, V{}, MAX_LEV)), cur_level(0) {
+  for (int i = 0; i <= MAX_LEV; ++i) {
+    head->forward[i] = tail;
+  }
+}
+
 template <typename K, typename V, typename Comp>
-auto SkipList<K, V, Comp>::find(K key) -> V {}
+auto SkipList<K, V, Comp>::random_level() -> int {
+  int level = 0;
+  while ((rng() & S) < PS)
+    ++level;
+  return level > MAX_LEV ? MAX_LEV : level;
+}
+
 template <typename K, typename V, typename Comp>
-auto SkipList<K, V, Comp>::insert(K key, V value) -> bool {}
+auto SkipList<K, V, Comp>::find(K key) -> std::pair<Nptr, bool> {
+  Nptr tmp = head;
+  for (int i = cur_level; i >= 0; --i) {
+    while (tmp->forward[i] != tail && cmp(tmp->forward[i]->key, key))
+      tmp = tmp->forward[i];
+    update[i] = tmp;
+  }
+  if (tmp->forward[0] == tail || tmp->forward[0]->key != key) {
+    return {tmp, false};
+  }
+  return {tmp, true};
+}
+
 template <typename K, typename V, typename Comp>
-auto SkipList<K, V, Comp>::erase(K key) -> bool {}
+auto SkipList<K, V, Comp>::insert(K key, V value) -> bool {
+  auto [tmp, status] = find(key);
+  operator[](key) = value;
+  return true;
+}
+
 template <typename K, typename V, typename Comp>
-auto SkipList<K, V, Comp>::begin() -> V {}
+auto SkipList<K, V, Comp>::erase(K key) -> bool {
+  auto [tmp, status] = find(key);
+  if (!status) {
+    return false;
+  }
+  for (int i = 0; i <= cur_level; ++i) {
+    if (update[i]->forward[i] != tmp->forward[i])
+      break;
+    update[i]->forward[i] = tmp->forward[i]->forward[i];
+  }
+  return true;
+}
+
 template <typename K, typename V, typename Comp>
-auto SkipList<K, V, Comp>::end() -> V {}
+auto SkipList<K, V, Comp>::begin() -> Iter {
+  return Iter{head->forward[0]};
+}
 template <typename K, typename V, typename Comp>
-auto SkipList<K, V, Comp>::operator[](K key) -> V& {}
+auto SkipList<K, V, Comp>::end() -> Iter {
+  return tail;
+}
+
+template <typename K, typename V, typename Comp>
+auto SkipList<K, V, Comp>::operator[](K key) -> V & {
+  auto [tmp, status] = find(key);
+  if (status) {
+    return tmp->forward[0]->value;
+  }
+  auto node_level = [&](int res) {
+    if (res > cur_level) {
+      res = ++cur_level;
+      update[res] = head;
+    }
+    return res;
+  }(random_level());
+  Nptr new_node = std::make_shared<SkipListNode>(key, V{}, node_level);
+  for (int i = node_level; i >= 0; --i) {
+    new_node->forward[i] = update[i]->forward[i];
+    update[i]->forward[i] = new_node;
+  }
+  return new_node->value;
+}
+
+template <typename K, typename V, typename Comp>
+auto SkipList<K, V, Comp>::contains(K key) -> bool {
+  auto [tmp, status] = find(key);
+  return status;
+}
 } // namespace mzi
--- SkipList/test.cpp
+++ SkipList/test.cpp
@@ -7,29 +7,52 @@
 
 constexpr int N = 1e5;
 auto main() -> int {
+  std::cout << std::fixed << std::setprecision (7);
+  std::clock_t s = std::clock();
   std::map<int, int> M;
   mzi::SkipList<int, int> L;
 
-  std::clock_t s = std::clock();
+  std::clock_t s0 = std::clock();
   std::mt19937 rng(std::random_device{}());
   for (int i = 0; i < N; ++i) {
     int key = rng(), value = rng();
     M[key] = value;
     L[key] = value;
+    // L.insert(key, value);
   }
+
+  std::clock_t e0 = std::clock();
+  std::cout << "Insert Time elapsed: " << (double)(e0 - s0) / CLOCKS_PER_SEC << std::endl;
+
+  std::clock_t s1 = std::clock();
+  auto iter = L.begin();
+  for (auto [k, v] : M) {
+    assert(k == (*iter).first);
+    assert(v == (*iter).second);
+    ++iter;
+  }
+
+  std::clock_t e1 = std::clock();
+  std::cout << "For Time elapsed: " << (double)(e1 - s1) / CLOCKS_PER_SEC << std::endl;
+
+  std::clock_t s2 = std::clock();
   for (int i = 0; i < N; ++i) {
     int key =rng();
     if (i & 1) {
       L.erase(key);
       M.erase(key);
     } else {
-      int r1 = L.find(key) ? L[key] : 0;
+      int r1 = L.find(key).second ;
       int r2 = M.count(key) ? M[key] : 0;
       assert(r1 == r2);
     }
   }
+  std::clock_t e2 = std::clock();
+  std::cout << "Erase And Find Time elapsed: " << (double)(e2 - s2) / CLOCKS_PER_SEC << std::endl;
+
+
   std::clock_t e = std::clock();
-  std::cout << std::fixed << std::setprecision (7);
-  // about 0.2s/
   std::cout << "Time elapsed: " << (double)(e - s) / CLOCKS_PER_SEC << std::endl;
+  // about 0.2s/
+
 }
